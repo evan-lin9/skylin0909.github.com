f685073fc1043214bc683674b8a183a333e85c81
梁朝伟 <470035056@qq.com>
2017-07-09 14:36:28 +0800

    内容更新

--- js/easing.js
+++ js/easing.js
@@ -1,16 +1,3 @@
-/*
- * jQuery EasIng v1.1.2 - http://gsgd.co.uk/sandbox/jquery.easIng.php
- *
- * Uses the built In easIng capabilities added In jQuery 1.1
- * to offer multiple easIng options
- *
- * Copyright (c) 2007 George Smith
- * Licensed under the MIT License:
- *   http://www.opensource.org/licenses/mit-license.php
- */
-
-// t: current time, b: begInnIng value, c: change In value, d: duration
-
 jQuery.extend( jQuery.easing,
 {
 	easeInQuad: function (x, t, b, c, d) {
